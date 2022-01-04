36c3b8b9360ebc7e601d2565f6a5eaf3d5e1346c
AntWorkingV@outlook.com <AntWorkingV@outlook.com>
2022-01-04 20:12:11 +0300

    Добавьте файлы проекта.

---
+++ RevSignalRClient/Models/NeuronetInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevSignalRClient.Models
+{
+    public class NeuronetInformation
+    {
+        public int Id { get; set; }
+        public byte Age { get; set; }
+        public string Gender { get; set; }
+        public string Mood { get; set; }
+    }
+}
---
+++ RevSignalRClient/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+using RevSignalRClient.Models;
+using System.Configuration;
+using System.Text.Json;
+
+string hubUrl = ConfigurationManager.AppSettings.Get("LOGGING_HUB_URL");
+HubConnection connection = new HubConnectionBuilder()
+                           .WithUrl(hubUrl)
+                           .WithAutomaticReconnect()
+                           .Build();
+
+connection.Closed += async (error) =>
+{
+    await Task.Delay(new Random().Next(0, 5) * 1000);
+    await connection.StartAsync();
+};
+
+connection.On<string>("TakeLog", (message) =>
+{
+    NeuronetInformation? ni = JsonSerializer.Deserialize<NeuronetInformation>(message);
+    Console.WriteLine($"Id: {ni.Id}\n" +
+                      $"Gender: {ni.Gender}\n" +
+                      $"Age: {ni.Age}\n" +
+                      $"Mood: {ni.Mood}");
+    Console.WriteLine();
+});
+
+try
+{
+    await connection.StartAsync();
+    Console.WriteLine("Connection to hub has been established!");
+    while (true) { }
+}
+catch(Exception)
+{
+    Console.WriteLine("Can't connect to the hub");
+}
+
+
+
